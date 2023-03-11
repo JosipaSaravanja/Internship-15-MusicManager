b6d9f051aa6d0ec08088930e45a7be16375ebb82
Josipa Šaravanja <josipa.saravanja06@gmail.com>
2023-03-11 22:31:56 +0100

    added limit od 10 albums

--- music-manager/src/App.js
+++ music-manager/src/App.js
@@ -5,36 +5,56 @@ import { newAlbum, albumsCollection } from "./data";
 import React from "react";
 
 function App() {
-  const sortAlbums=(array)=>{ //optimize???
-    return array.sort((a, b) => {
-      if (a.name > b.name) {
-        return 1;
-    }
-    if (a.name < b.name) {
-        return -1;
-    }
-    return 0;
-    }).sort((a, b) => {
-      if (a.author > b.author) {
-        return 1;
-    }
-    if (a.author < b.author) {
-        return -1;
-    }
-    return 0;
-    }).sort((a, b) => b.releaseDate-a.releaseDate)
-  }
+  const sortAlbums = (array) => {
+    //optimize???
+    return array
+      .sort((a, b) => {
+        if (a.name > b.name) {
+          return 1;
+        }
+        if (a.name < b.name) {
+          return -1;
+        }
+        return 0;
+      })
+      .sort((a, b) => {
+        if (a.author > b.author) {
+          return 1;
+        }
+        if (a.author < b.author) {
+          return -1;
+        }
+        return 0;
+      })
+      .sort((a, b) => b.releaseDate - a.releaseDate);
+  };
 
-  const removeAlbum=(id)=>{  
-    setData(prev => [...prev.filter(album => album.id !== id)])
-  }
+  const removeAlbum = (id) => {
+    setData((prev) => [...prev.filter((album) => album.id !== id)]);
+  };
 
-  const addAlbum=(name, author, genre, releaseDate)=>{
-    setData(prev=>sortAlbums([...prev, new newAlbum(name, author, genre,  new Date(releaseDate).getFullYear(), new Date(releaseDate))]));
-  }
-  
+  const addAlbum = (name, author, genre, releaseDate) => {
+    if (data.lenght >= 10) {
+      alert("Ne možete unijeti više od 10 albuma");
+    } else {
+      setData((prev) =>
+        sortAlbums([
+          ...prev,
+          new newAlbum(
+            name,
+            author,
+            genre,
+            new Date(releaseDate).getFullYear(),
+            new Date(releaseDate)
+          ),
+        ])
+      );
+    }
+  };
+
   const [data, setData] = React.useState(sortAlbums(albumsCollection));
-  
+
+console.log(data)
   return (
     <div className="App">
       <div className="content">
